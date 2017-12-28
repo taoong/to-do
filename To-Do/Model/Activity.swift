bf83c113c61a6117ccc889acd6259c9ed7f9b89d
Tao Ong <taoong@berkeley.edu>
2017-12-28 23:09:29 +0800

    Setting up UI elements

---
+++ To-Do/Model/Activity.swift
@@ -0,0 +1,15 @@
+//
+//  Activity.swift
+//  To-Do
+//
+//  Created by Tao Ong on 12/17/17.
+//  Copyright © 2017 Tao Ong. All rights reserved.
+//
+
+import Foundation
+
+class Activity {
+    var title : String
+    var dueDate : Date
+    var description : String
+}
